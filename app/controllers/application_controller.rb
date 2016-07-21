94ff8b1d6aac02016bc76983907d9e02a2a354e1
mreed19 <michael.reed315@gmail.com>
2016-07-21 14:50:43 -0400

    Allow users to sign in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def authenticate
+    redirect_to new_session_path unless signed_in?
+  end
+
+  def signed_in?
+    session[:current_email].present?
+  end
 end
